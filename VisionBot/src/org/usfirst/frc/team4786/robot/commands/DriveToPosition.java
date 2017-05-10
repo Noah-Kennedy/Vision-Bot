eabd08b45bf75c823165b153d8b1270d5c2890d4
Noah-Kennedy <Nomaxx117@gmail.com>
2017-05-10 15:44:17 -0500

    Added support for DriveToPosition

---
+++ VisionBot/src/org/usfirst/frc/team4786/robot/commands/DriveToPosition.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team4786.robot.commands;
+
+import org.usfirst.frc.team4786.robot.Robot;
+import org.usfirst.frc.team4786.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveToPosition extends Command {
+
+	private double targetPosition;
+
+	public DriveToPosition(double distance) {
+		// We require the driveTrain to drive, obviously!!!!
+		requires(Robot.driveTrain);
+
+		// So we can use this variable in the execute() function
+		targetPosition = distance * RobotMap.fudgeFactor;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		// Let's drive!
+		Robot.driveTrain.driveToPositionInit(targetPosition);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Robot.driveTrain.driveToPositionIsFinished();
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.driveTrain.driveToPositionEnd();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+}
